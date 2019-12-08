4caeb1b853d1fef9078c679064921852863c4cfc
raul sanz <estraulsanz@gmail.com>
2019-12-07 23:01:46 -0600

    validation all inputs

--- src/components/ModalCreateEmployee.vue
+++ src/components/ModalCreateEmployee.vue
@@ -7,14 +7,36 @@
         </q-card-section>
 
         <q-card-section>
-          <q-input filled dense v-model="newEmployee.name" autofocus  class="q-mb-md" label="Name"/>
-          <q-input filled dense v-model="newEmployee.middle_name" class="q-mb-md" label="Middle Name" />
-          <q-input filled dense v-model="newEmployee.last_name" class="q-mb-md" label="Last Name"/>
+          <q-input
+            ref="name" 
+            filled 
+            dense 
+            v-model="newEmployee.name" 
+            autofocus  
+            class="q-mb-md" 
+            label="Name" 
+            :rules="[val => !!val || 'Field is required']"/>
+          <q-input
+            ref="middle_name" 
+            filled 
+            dense 
+            v-model="newEmployee.middle_name" 
+            class="q-mb-md" 
+            label="Middle Name"  
+            :rules="[val => !!val || 'Field is required']"/>
+          <q-input
+            ref="last_name" 
+            filled 
+            dense 
+            v-model="newEmployee.last_name" 
+            class="q-mb-md" 
+            label="Last Name" 
+            :rules="[val => !!val || 'Field is required']"/>
         </q-card-section>
 
         <q-card-actions align="right" class="text-primary">
           <q-btn flat color="red" label="Cancel" v-close-popup />
-          <q-btn flat color="green" label="Create employee" v-close-popup  @click="create"/>
+          <q-btn flat color="green" label="Create employee" @click="create"/>
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -45,12 +67,29 @@ export default {
     ...mapMutations('data',['setModalCreateEmployee','setNewEmployee']),
     ...mapActions('data',['postEmployees']),
     async create(){
-      this.$q.loading.show()
-      this.newEmployee.branch = this.branchDetails.id
-      console.log(this.newEmployee)
-      let {data} = await this.postEmployees(this.newEmployee)
-      this.setNewEmployee(data)
-      data ? this.$q.loading.hide() : null
+      const error = [];
+      for (let [key, obj] of Object.entries(this.$refs)) {
+        obj.validate();
+        if (obj.hasError) {
+          error.push(obj);
+        }
+      }
+      if (error.length > 0) {
+        this.$q.notify({
+          color: "negative",
+          message: "Please complete fields",
+          position: "top-right"
+        });
+      }else{
+        this.$q.loading.show()
+        this.newEmployee.branch = this.branchDetails.id
+        console.log(this.newEmployee)
+        let {data} = await this.postEmployees(this.newEmployee)
+        this.setNewEmployee(data)
+        data ? this.$q.loading.hide() : null
+        this.setModalCreateEmployee(false)
+      }
+      
     }
   },
 
